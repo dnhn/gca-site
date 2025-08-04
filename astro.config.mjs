e8e61ba2d5b00e449ebe4916ebb25695884df556
nhan.dev <hey@nhan.dev>
2025-08-04 12:24:59 +0700

    Thanks

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,11 @@ export default defineConfig({
 		starlight({
 			title: 'Git Commit Audio',
 			description: 'Add a spark of joy to coding—hear a surprise sound every time you commit.',
+			favicon: 'favicon.png',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/dnhn/gca' }],
 			sidebar: [
 				{ slug: 'docs' },
+				{ slug: 'docs/thanks' },
 			],
 			customCss: ['./src/styles/global.css'],
 		}),
---
+++ src/content/docs/docs/thanks.md
@@ -0,0 +1,7 @@
+---
+title: Thanks
+---
+
+[Illustration](https://icons8.com/illustrations/illustration/bell) and
+[Commit Git](https://icons8.com/icon/bZyfp8hP1K1e/commit-git) icon by
+[Icons8](https://icons8.com)
