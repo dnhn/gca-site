6400660e34424b5d6280e434f76e7a352b3369f2
nhan.dev <hey@nhan.dev>
2025-08-05 13:10:49 +0700

    Thanks

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,22 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			credits: true,
-			title: 'Git Commit Audio',
-			titleDelimiter: '—',
-			description: 'Add a spark of joy to coding—hear a surprise sound every time you commit.',
-			logo: {
-				dark: './src/assets/logo-invert.svg',
-				light: './src/assets/logo.svg',
-			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/dnhn/gca' }],
-			sidebar: ['intro', 'thanks'],
-			customCss: ['./src/styles/global.css'],
-		}),
-	],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  integrations: [
+    starlight({
+      credits: true,
+      title: 'Git Commit Audio',
+      titleDelimiter: '—',
+      description: 'Add a spark of joy to coding—hear a surprise sound every time you commit.',
+      logo: {
+        dark: './src/assets/logo-invert.svg',
+        light: './src/assets/logo.svg',
+      },
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/dnhn/gca' }],
+      sidebar: ['intro', 'thanks'],
+      customCss: ['./src/styles/global.css'],
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/content/docs/thanks.md
+++ src/content/docs/thanks.md
@@ -2,6 +2,9 @@
 title: Thanks
 ---
 
-[Illustration](https://icons8.com/illustrations/illustration/bell) and
-[Commit Git](https://icons8.com/icon/bZyfp8hP1K1e/commit-git) icon by
-[Icons8](https://icons8.com)
+- [GitHub Copilot](https://gh.io/copilot) for co-piloting
+- Audio from [Freesound](https://freesound.org/s/324894)
+- [Illustration](https://icons8.com/illustrations/illustration/bell) and
+[icon](https://icons8.com/icon/bZyfp8hP1K1e/commit-git) from [Icons8](https://icons8.com)
+- Logo from [ZURB Foundation](https://github.com/zurb/foundation-icon-fonts)
+- Cover image from [og.new](https://og.new)
